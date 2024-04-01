606f91d648ffc80b3b078806c017086bce1bab63
miniite <ninanittyeipe@gmail.com>
2024-04-01 16:09:49 +0530

    login_updated

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include 
-from .views import root,prof, trading, into_insight
+from .views import root, loginning, joinning, prof, trading, into_insight
 
 
 urlpatterns = [
     path("",root),
+    path('login/', loginning, name="login"),
+    path('signup/', joinning, name="signup"),
     path('profile/', prof, name="profile"),
     path('trade/', trading, name="trade"),
     path('insight/', into_insight, name="insight")
--- core/views.py
+++ core/views.py
@@ -1,10 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 # Create your views here.
 # Calling Frontend
 def root(request):
     return render(request,"index.html")
-    
+
+def loginning(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page
+            return redirect('index.html')
+        else:
+            # Return an invalid login message
+            messages.error(request, 'Invalid email or password. Please try again.')
+    return render(request, 'login.html')
+
+
+def joinning(request):
+    return render(request,"signup.html")
+
 def prof(request):
     return render(request,"profile.html")
 
